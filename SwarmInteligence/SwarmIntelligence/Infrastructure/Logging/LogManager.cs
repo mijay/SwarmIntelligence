b3cabdcfad8257f9525bdbf8fc9014f18769414e
Dmitry Kononchuk <kononchuk.dmitry@gmail.com>
2011-09-25 21:41:57 +0600

    начал писать обработку очереди логгирования

--- SwarmInteligence/SwarmIntelligence/Infrastructure/Logging/LogManager.cs
+++ SwarmInteligence/SwarmIntelligence/Infrastructure/Logging/LogManager.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.Concurrent;
 using SwarmIntelligence.Core;
 
 namespace SwarmIntelligence.Infrastructure.Logging
@@ -6,6 +10,8 @@ namespace SwarmIntelligence.Infrastructure.Logging
 	public class LogManager
 	{
 		private readonly ConcurrentQueue<TmpLogRecord> logAppendQueue = new ConcurrentQueue<TmpLogRecord>();
+		private readonly ChunkedArray<LogRecord> logRecords = new ChunkedArray<LogRecord>(10 * 1024);
+		private long logRecordIndex = -1;
 
 		public LogManager()
 		{
@@ -13,5 +19,26 @@ namespace SwarmIntelligence.Infrastructure.Logging
 		}
 
 		public ILog Log { get; private set; }
+
+		private void ProcessLog()
+		{
+			if(logAppendQueue.IsEmpty)
+				return;
+
+			//var firstExclusiveIndex = logRecordIndex;
+			IEnumerable<LogRecord> newRecords = DequeueAllAppended();
+			//var lastInclusiveIndex = logRecordIndex;
+
+			logRecords.Append(newRecords);
+		}
+
+		private IEnumerable<LogRecord> DequeueAllAppended()
+		{
+			var newRecords = new List<LogRecord>(logAppendQueue.Count);
+			TmpLogRecord tmpLogRecord;
+			while(logAppendQueue.TryDequeue(out tmpLogRecord))
+				newRecords.Add(new LogRecord(++logRecordIndex, tmpLogRecord));
+			return newRecords;
+		}
 	}
 }
--- SwarmInteligence/SwarmIntelligence/Infrastructure/Logging/LogRecord.cs
+++ SwarmInteligence/SwarmIntelligence/Infrastructure/Logging/LogRecord.cs
@@ -5,13 +5,13 @@ namespace SwarmIntelligence.Infrastructure.Logging
 	public struct LogRecord
 	{
 		public readonly object[] arguments;
-		public readonly ulong id;
+		public readonly long id;
 		public readonly string type;
 
-		internal LogRecord(ulong id, TmpLogRecord tmpLogRecord)
+		internal LogRecord(long id, TmpLogRecord tmpLogRecord)
 			: this()
 		{
-			Contract.Requires(id > 0 && tmpLogRecord.eventArgs != null && !string.IsNullOrEmpty(tmpLogRecord.eventType));
+			Contract.Requires(id >= 0 && tmpLogRecord.eventArgs != null && !string.IsNullOrEmpty(tmpLogRecord.eventType));
 			this.id = id;
 			type = tmpLogRecord.eventType;
 			arguments = tmpLogRecord.eventArgs;
