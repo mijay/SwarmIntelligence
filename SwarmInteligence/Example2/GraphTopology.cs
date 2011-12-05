3a80320ceea68c192c9615dac32c42195cc62d8a
Sergey Kalistratov <sergey.kalistratov@gmail.com>
2011-12-06 01:37:52 +0600

    Добавил топологию для графа с весами ребер

---
+++ SwarmInteligence/Example2/GraphTopology.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SILibrary.General;
+using SwarmIntelligence.Core.Space;
+
+namespace Example2
+{
+    public class GraphTopology: BiConnectedTopologyBase<SimpleCoordinates>
+    {
+        private List<SimpleCoordinates> vertexes = new List<SimpleCoordinates>();
+        private Dictionary<SimpleCoordinates, Dictionary<SimpleCoordinates, int>> edges = new Dictionary<SimpleCoordinates, Dictionary<SimpleCoordinates, int>>();
+
+        public GraphTopology(List<SimpleCoordinates> vertexes, Dictionary<SimpleCoordinates, Dictionary<SimpleCoordinates, int>> edges)
+        {
+            this.vertexes = vertexes;
+            this.edges = edges;
+        }
+
+        public override bool Lays(SimpleCoordinates coord)
+        {
+            return vertexes.Contains(coord);
+        }
+
+        protected override IEnumerable<SimpleCoordinates> GetNeighbours(SimpleCoordinates coord)
+        {
+            var neighbours = edges[coord];
+
+            return (from neighbour in neighbours where neighbour.Value != 0 select neighbour.Key).ToList();
+        }
+}
+}
---
+++ SwarmInteligence/Example2/SimpleCoordinates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SwarmIntelligence.Core.Space;
+
+namespace Example2
+{
+    public struct SimpleCoordinates: ICoordinate<SimpleCoordinates>
+    {
+        public readonly int vertex;
+
+        public SimpleCoordinates(int vertex)
+        {
+            this.vertex = vertex;
+        }
+
+        public bool Equals(SimpleCoordinates other)
+        {
+            return this.vertex == other.vertex;
+        }
+
+        public SimpleCoordinates Clone()
+        {
+            return new SimpleCoordinates(vertex);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (vertex * 397);
+            }
+        }
+    }
+}
